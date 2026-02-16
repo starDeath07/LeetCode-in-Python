ec882938163c3dc788d597f1b69e0cd7e983c6c6
deathStar <deathstar07@protonmail.com>
2026-02-16 16:08:10 +0530

    Added solution for Leetcode 190. Reverse Bits

---
+++ Leetcode_190.py
@@ -0,0 +1,10 @@
+class Solution:
+    def reverseBits(self, n: int) -> int:
+        ans = 0
+
+        for _ in range(32):
+            ans <<= 1
+            ans = ans | (n & 1)
+            n >>= 1
+
+        return ans
