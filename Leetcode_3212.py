df76b661115f3200313116b80cebf19fb76308cd
deathStar <deathstar07@protonmail.com>
2026-03-19 16:53:53 +0530

    Added solution for Leetcode 3212. Count Submatrices With Equal Frequency of X and Y with updated solution also.

--- Leetcode_3212.py
+++ Leetcode_3212.py
@@ -27,3 +27,25 @@ class Solution:
                     ans += 1
 
         return ans
+
+
+# Another better Solution
+
+
+class Solution:
+    def numberOfSubmatrices(self, grid: list[list[str]]) -> int:
+        ans, m, n = 0, len(grid), len(grid[0])
+        up: list[list[int]] = [[0, 0] for _ in range(n)]
+
+        for i in range(m):
+            curr = [0, 0]
+            for j in range(n):
+                up[j][0] += 1 if grid[i][j] == "X" else 0
+                up[j][1] += 1 if grid[i][j] == "Y" else 0
+
+                curr[0] += up[j][0]
+                curr[1] += up[j][1]
+
+                if curr[0] > 0 and curr[0] == curr[1]:
+                    ans += 1
+        return ans
