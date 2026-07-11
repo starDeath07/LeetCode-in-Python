9d5d4ab0e4b4e40e0f3c36b19c6e8a7524fc467d
deathStar <deathstar07@protonmail.com>
2026-07-11 15:29:36 +0530

    Added solution for Leetcode 3914. Minimum Operations to Make Array Non Decreasing

---
+++ Leetcode_3914.py
@@ -0,0 +1,9 @@
+class Solution:
+    def minOperations(self, nums: list[int]) -> int:
+        n = len(nums)
+        ans = 0
+        for i in range(1, n):
+            if nums[i - 1] > nums[i]:
+                ans += nums[i - 1] - nums[i]
+
+        return ans
