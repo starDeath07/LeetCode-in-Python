cf6d359c67f043881f4c82f90ba2c4ec661cf8e8
starDeath07 <135846839+starDeath07@users.noreply.github.com>
2026-09-14 14:55:17 +0530

    Added solution for Leetcode 836. Rectangle Overlap

---
+++ Leetcode_836.py
@@ -0,0 +1,9 @@
+class Solution:
+    def isRectangleOverlap(self, rec1: list[int], rec2: list[int]) -> bool:
+        left = max(rec1[0], rec2[0])
+        right = min(rec1[2], rec2[2])
+
+        bottom = max(rec1[1], rec2[1])
+        top = min(rec1[3], rec2[3])
+
+        return left < right and bottom < top
